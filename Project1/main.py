d89b37845164ee96be42cc0e71b700f16e27439b
Ahmed-Aj <60852013+Ahmed-Aj@users.noreply.github.com>
2021-02-23 22:07:58 +0000

    Add files via upload

--- Project1/main.py
+++ Project1/main.py
@@ -1,1 +1,33 @@
+end = False
+print("""
+***********
+Calculator
+***********""")
 
+print("Menu:\n"
+      " 1) Sum\n "
+      "2) Subtractions\n "
+      "3) Division\n "
+      "4) Multiplication\n "
+      "5) Exit\n ")
+while not end:
+    opt = int(input("Option:"))
+    if opt == 1:
+        num1 = int(input("Enter the first the number(integer):"))
+        num2 = int(input("Enter the second the number(integer):"))
+        print("Result: ", num1 + num2)
+    elif opt == 2:
+        num1 = int(input("Enter the first the number(integer):"))
+        num2 = int(input("Enter the second the number(integer):"))
+        print("Result: ", num1 - num2)
+    elif opt == 3:
+        num1 = int(input("Enter the first the number(integer):"))
+        num2 = int(input("Enter the second the number(integer):"))
+        print("Result: ", num1 / num2)
+    elif opt == 4:
+        num1 = int(input("Enter the first the number(integer):"))
+        num2 = int(input("Enter the second the number(integer):"))
+        print("Result: ", num1 * num2)
+    elif opt == 5:
+        print("Good bye!")
+        end = True
